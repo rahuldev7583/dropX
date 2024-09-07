5b7210a58c806da4de8dd633ff47b2b23c6d7aeb
Rahul Dev <rahuldev7583@gmail.com>
2024-09-08 01:09:06 +0530

    airdrop and send sol implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,34 @@
-import Image from "next/image";
+"use client";
+import Airdrop from "@/components/Airdrop";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 export default function Home() {
-  return <div className="">AirDrift</div>;
+  const SOL_API = process.env.NEXT_PUBLIC_SOL_API || "";
+  return (
+    <div className="">
+      <ConnectionProvider endpoint={SOL_API}>
+        <WalletProvider wallets={[]} autoConnect>
+          <WalletModalProvider>
+            <div className="flex mt-4 ml-2 md:ml-8">
+              <h1 className="text-3xl font-bold">AirDrift</h1>
+              <div className="md:flex ml-12 md:ml-[60%] md:space-x-16">
+                <WalletMultiButton />
+                <WalletDisconnectButton />
+              </div>
+            </div>
+            <Airdrop />
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </div>
+  );
 }
